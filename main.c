d6f7d5fb4efd863c2dff2d482ee121c899782b0b
UserAlexUser <93340879+UserAlexUser@users.noreply.github.com>
2021-11-07 10:46:17 +0000

    Create new func to control LED

--- main.c
+++ main.c
@@ -3,67 +3,46 @@
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
 #include "boards.h"
-#define first_LED   0
-#define second_LED  1
-#define third_LED   2
-#define fourth_LED  3
-
-#define INIT_LED    0
-#define ON_LED      1
-#define OFF_LED     2
-#define TOGGLE_LED  3
 
 #define NRF_GPIO_PIN_MAP(port, pin) (((port) << 5) | ((pin) & 0x1F))
 
 static const uint8_t led_list[LEDS_NUMBER] = LEDS_LIST;
 
-void led_control(uint32_t led_idx,uint8_t cmd)
+void on_led(uint32_t led_idx)
 {
-    if(cmd == INIT_LED)
-    {   
-        nrf_gpio_cfg_output(led_list[led_idx]);
-        led_control(led_list[led_idx],OFF_LED);
-    }    
-    else if (cmd == ON_LED)
-        nrf_gpio_pin_write(led_list[led_idx], 1);
-    else if (cmd == OFF_LED)
-        nrf_gpio_pin_write(led_list[led_idx], 0);
-    else if (cmd == TOGGLE_LED)
-        nrf_gpio_pin_toggle(led_list[led_idx]);
+    nrf_gpio_pin_write(led_list[led_idx], 1);
 }
+
+void off_led(uint32_t led_idx)
+{
+    nrf_gpio_pin_write(led_list[led_idx], 0);
+}
+
+void toggle_led(uint32_t led_idx)
+{
+    nrf_gpio_pin_toggle(led_list[led_idx]);
+}
+
+void init_led(uint32_t led_idx)
+{
+    nrf_gpio_cfg_output(led_list[led_idx]);
+    off_led(led_list[led_idx]);  
+}
+
 int main(void)
 {
     bsp_board_init(BSP_INIT_LEDS);
-    char cnt_1 = 6*2;
-    char cnt_2 = 5*2;
-    char cnt_3 = 8*2;
-    char cnt_4 = 9*2;
-    /* Toggle LEDs. */
+    char cnt_led[4] = {6*2, 5*2, 8*2, 9*2};
+    char number_led[4] = {0, 1, 2, 3};
     while (true)
     {
-        for(int i=0; i<cnt_1; i++)
-            {   
-                led_control(first_LED,TOGGLE_LED);
-                nrf_delay_ms(250);
-            }
-        for(int i=0; i<cnt_2; i++)
-            {   
-                if(i%2)
-                led_control(second_LED,ON_LED);
-                else
-                led_control(second_LED,OFF_LED);
-                nrf_delay_ms(250);
-            }
-        for(int i=0; i<cnt_3; i++)
-            {   
-                nrf_gpio_pin_toggle(led_list[third_LED]);
-                //bsp_board_led_invert(third_LED);   
-                nrf_delay_ms(250);
-            }
-        for(int i=0; i<cnt_4; i++)
+        for(int i=0; 1<4; i++)
+        {
+            for(int j=0; j<cnt_led[i]; j++)
             {
-                bsp_board_led_invert(fourth_LED); 
+                toggle_led(number_led[i]);
                 nrf_delay_ms(250);
             }
+        }
     }
 }
